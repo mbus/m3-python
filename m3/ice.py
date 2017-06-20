15dd09124549d5c61bf18a99226f354ba429440d
Andrew Lukefahr <lukefahr@umich.edu>
2017-06-20 17:40:48 -0400

    ICE:  backed timeout to 500ms b/c of cygwin

--- m3/ice.py
+++ m3/ice.py
@@ -250,9 +250,9 @@ class ICE(object):
         changed after this method is invoked.
         '''
 
-        #50ms timeout for serial to help catch runaway packets
-        # cygwin cannot support 5ms timeouts 
-        self.dev = serial.Serial(serial_device, baudrate, timeout=.05)
+        #500ms timeout for serial to help catch runaway packets
+        # cygwin cannot support 5ms or 50ms timeouts 
+        self.dev = serial.Serial(serial_device, baudrate, timeout=0.5)
         if self.dev.isOpen():
             logger.info("Connected to serial device at " + self.dev.portstr + 
                 " at " + str(baudrate) + " baud")
